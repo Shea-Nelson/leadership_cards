e645df6069f58503b0597cd797293463c91108f5
Shea Nelson <shea.nelson@icloud.com>
2016-02-22 10:00:19 -0500

    Readjusted for SharePoint Content
    changed links to absolute links took out body tags and adjusted
    stylings for SharePoint

--- js/index.js
+++ js/index.js
@@ -1,26 +1,34 @@
 $(window).load(function() {
   $(".flipCard").css("cursor", "pointer");
   $(".moreInfo").click(function(){
-    $(".main").addClass("blur");
+    $("main").addClass("blur");
     $(".popup").fadeToggle(200);
-    $(".body").addClass("blur");
+
     $(".popup").empty();
     $(".overlay").addClass("dimBackground");
-    $(".areaText").addClass("hidden");
-
+    $(".areaText").addClass("hide");
 
     var empPosition = "#" +
       $(this).closest('.flipCard').attr('id') +
       "Bio";
     $(".popup").append($(empPosition).html());
-     });
-    $(".popup").click(function(){
-     $(".main").removeClass("blur");
-     $("body").removeClass("blur");
-
-    $(".popup").fadeToggle(150);
-    $("body").removeClass("blur");
-    $(".overlay").removeClass("dimBackground");
-    $(".areaText").removeClass("hidden");
   });
+
+  $(".popup").click(function(){
+   $("main").removeClass("blur");
+   $("body").removeClass("blur");
+
+   $(".popup").fadeToggle(150);
+   $(".overlay").removeClass("dimBackground");
+   $(".areaText").removeClass("hide");
+ });
 });
+
+/* function popupClose() {
+  $("main").removeClass("blur");
+  $("body").removeClass("blur");
+
+  $(".popup").fadeToggle(150);
+  $(".overlay").removeClass("dimBackground");
+  $(".areaText").removeClass("hide");
+}; */
